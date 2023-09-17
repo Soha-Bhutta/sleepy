47b2e591569db9af6bcaee5e9c3aaeee2c9457f2
Soha Bhutta <sohabhutta@MacBook-Pro-115.local>
2023-09-17 04:31:32 -0400

    final submission 1

--- app.py
+++ app.py
@@ -1,12 +1,47 @@
-from flask import Flask
+from flask import Flask, render_template, Response
+import cv2
+from ultralytics import YOLO
+import base64
 
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder ='templates')
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
+def generate_frames():
+    cap = cv2.VideoCapture(0)
+    model = YOLO('/Users/sohabhutta/PycharmProjects/theBackend3/sleepymodel.pt')
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        results = model(frame)
+
+        box = results[0].boxes
+        label = box.cls
+        class_labels_strings = [results[0].names[int(labe.cpu().numpy())] for labe in label]
+        print(class_labels_strings)
+        if "Asleep" in class_labels_strings:
+            print("Alert: 'asleep' detected!")
+
+        annotated_frame = results[0].plot()
+
+
+
+        ret, buffer = cv2.imencode('.jpg', annotated_frame)
+        frame = buffer.tobytes()
+        #frame = base64.b64encode(buffer).decode('utf-8')
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+    cap.release()
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- sleepy.py
+++ sleepy.py
@@ -1,29 +1,46 @@
-import torch
-from matplotlib import pyplot as plt
-import numpy as np
 import cv2
 from ultralytics import YOLO
-import pandas
+import playsound
+import random
+import random
 
-# model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+import cv2
+import playsound
+from ultralytics import YOLO
 
 cap = cv2.VideoCapture(0)
+model = YOLO('/Users/sohabhutta/PycharmProjects/theBackend3/sleepymodel.pt')
 
 while True:
     ret, frame = cap.read()
-
-    model = YOLO('/Users/christinazhang/Documents/vscode/hackmit/sleepymodel.pt')
     results = model(frame)
+    count = 45
 
     box = results[0].boxes
     label = box.cls
     class_labels_strings = [results[0].names[int(labe.cpu().numpy())] for labe in label]
-    print(class_labels_strings)
     if "Asleep" in class_labels_strings:
-        print("Alert: 'asleep' detected!")
+        while count > 0:
+            ret, frame = cap.read()
+            results = model(frame)
+            box = results[0].boxes
+            label = box.cls
+            class_labels_strings = [results[0].names[int(labe.cpu().numpy())] for labe in label]
+            if "Awake" in class_labels_strings:
+                break
+            annotated_frame = results[0].plot()
+            cv2.imshow('Webcam Feed', annotated_frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            count -= 1
+        if count == 0:
+            num = random.randint(1,2)
+            if num == 1:
+                playsound.playsound("/Users/sohabhutta/PycharmProjects/theBackend3/mystery1.mp3", True)
+            else:
+                playsound.playsound("/Users/sohabhutta/PycharmProjects/theBackend3/mystery2.mp3", True)
 
     annotated_frame = results[0].plot()
-
     cv2.imshow('Webcam Feed', annotated_frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
